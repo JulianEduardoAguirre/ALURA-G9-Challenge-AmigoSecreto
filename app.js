98f0b0fcf64fb404e9d299c04fb20fed812ab75a
Julián Eduardo Aguirre <aguirre.julian.eduardo@gmail.com>
2025-08-14 19:37:13 -0300

    creadas fciones agregarAmigo y sortearAmigo (v0.1)

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let arregloAmigos = [];
+let ulDom = document.getElementById("listaAmigos");
+let inputNombre = document.getElementById("amigo");
+
+function comprobarTexto(texto) {
+	return texto.length == 0;
+}
+
+function agregarAmigo() {
+	let textoIngresado = inputNombre.value;
+	// console.log(textoIngresado);
+	// console.log(comprobarTexto(textoIngresado));
+
+	if (!comprobarTexto(textoIngresado)) {
+		arregloAmigos.push(textoIngresado);
+		ulDom.innerHTML = '';
+
+		arregloAmigos.forEach((item) => {
+		let li = document.createElement("li");
+		li.innerText = item;
+		ulDom.appendChild(li);
+		inputNombre.value = '';
+
+	});
+
+	} else {
+		alert("¡Debe ingresar un nombre!");
+	}
+
+}
+
+function sortearAmigo() {
+
+	if (arregloAmigos.length == 0) {
+		alert("¡Primero debes ingresar el nombre de tus amigos!");
+	} else {
+		ulDom.innerHTML = '';
+    const indiceAleatorio = Math.floor(Math.random() * arregloAmigos.length);
+    let nombreSorteado =  arregloAmigos[indiceAleatorio];
+
+		let li = document.createElement("li");
+		li.innerText = nombreSorteado;
+		ulDom.appendChild(li);
+		inputNombre.value = '';
+
+	}
+
+
+}
